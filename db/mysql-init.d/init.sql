8a5c9677e34831abb27c5f45c43eabb77cd44ea6
Ji Seungmin <cswcsm02@gmail.com>
2023-04-02 16:24:41 +0900

    Add: sql init 파일 추가 및 mysql 환경 설정 cof 추가

---
+++ db/mysql-init.d/init.sql
@@ -0,0 +1,66 @@
+CREATE USER 'personal'@'localhost' IDENTIFIED BY 'personal';
+CREATE USER 'personal'@'%' IDENTIFIED BY 'personal';
+
+GRANT ALL PRIVILEGES ON *.* TO 'personal'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'personal'@'%';
+
+CREATE DATABASE IF NOT EXISTS personal DEFAULT CHARACTER SET utf8mb4 COLLATE utf8_general_ci DEFAULT ENCRYPTION='N';
+USE personal;
+
+drop table if exists comment;
+drop table if exists hibernate_sequence;
+drop table if exists likes;
+drop table if exists member;
+drop table if exists member_roles;
+drop table if exists project;
+
+create table comment (
+                         comment_id bigint not null,
+                         content varchar(255),
+                         member_id bigint,
+                         project_id bigint,
+                         primary key (comment_id)
+) engine=InnoDB;
+
+create table likes (
+                       likes_id bigint not null,
+                       mem_id bigint,
+                       pro_id bigint,
+                       member_id bigint,
+                       project_id bigint,
+                       primary key (likes_id)
+) engine=InnoDB;
+
+create table member (
+                        member_id bigint not null,
+                        type varchar(255),
+                        email varchar(255),
+                        introduction varchar(255),
+                        nickname varchar(255),
+                        password varchar(255),
+                        profile_image varchar(255),
+                        primary key (member_id)
+) engine=InnoDB;
+
+create table member_roles (
+                              member_member_id bigint not null,
+                              roles varchar(255)
+) engine=InnoDB;
+
+create table project (
+                         project_id bigint not null,
+                         category integer,
+                         comment_count integer,
+                         content varchar(255),
+                         image varchar(255),
+                         like_count integer,
+                         link_image varchar(255),
+                         link_title varchar(255),
+                         link_url varchar(255),
+                         title varchar(255),
+                         view_count integer,
+                         member_id bigint,
+                         primary key (project_id)
+) engine=InnoDB;
+
+
